14ca37ad375097ac054d8e30d2a2cab8755baa1e
Marvin Abante <abante.marvin@yahoo.com>
2023-02-04 21:18:18 +0800

    Modified Quote migration using uuid

--- database/migrations/2023_02_04_110327_create_quotes_table.php
+++ database/migrations/2023_02_04_110327_create_quotes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('quotes', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('title', 100);
             $table->text('content');
             $table->string('description', 255);
