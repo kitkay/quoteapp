d01358750dcbcf8437b79ac2ca2e9874b5101e6d
Marvin Abante <abante.marvin@yahoo.com>
2023-02-13 21:53:32 +0800

    Back to sanctum as authenticator

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,7 @@ use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Laravel\Sanctum\HasApiTokens;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
