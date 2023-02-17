bed0b87f0750eb383d8853890977ce622a1dcc07
marvin <marvin@finfiles.nl>
2023-02-17 08:23:17 +0800

    rename from index to app blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>QuoteApp</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
