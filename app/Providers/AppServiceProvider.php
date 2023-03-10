4c84bbc1e5d27730dbaadeca0fabf71600964a43
marvin <marvin@finfiles.nl>
2023-03-11 07:34:47 +0800

    Register RepositoryServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Passport::ignoreRoutes();
+        $this->app->register(RepositoryServiceProvider::class);
     }
 
     /**
